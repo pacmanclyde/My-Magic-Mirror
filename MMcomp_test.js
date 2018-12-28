7a141d36731efeb7b6b1c6b8c1451688edcd9124
pacmanclyde <46227686+pacmanclyde@users.noreply.github.com>
2018-12-28 14:21:14 -0500

    Create MMcomp_test.js

---
+++ MMcomp_test.js
@@ -0,0 +1,20 @@
+{
+			anytime: [
+				"Hey there sexy!"
+			],
+			morning: [
+				"Good morning, handsome!",
+				"Enjoy your day!",
+				"How was your sleep?"
+			],
+			afternoon: [
+				"Hello, beauty!",
+				"You look sexy!",
+				"Looking good today!"
+			],
+			evening: [
+				"Wow, you look hot!",
+				"You look nice!",
+				"Hi, sexy!"
+			]
+		}
